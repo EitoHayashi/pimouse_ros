fc39d9cdcf7eefa867bb38006b929637a487a882
EitoHayashi <dragon@arche1.co.jp>
2019-06-26 01:28:41 +0000

    Add travis CI yaml file3

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,7 +8,7 @@ sudo pip install rospkg
 
 #ros install
 cd ..
-git clone git@github.com:EitoHayashi/ros_setup_scripts_Ubuntu16.04_server.git
+git clone https://github.com/EitoHayashi/ros_setup_scripts_Ubuntu16.04_server.git
 cd ./ros_setup_scripts_Ubuntu16.04_server
 bash ./step0.bach
 bash ./step1.bash
